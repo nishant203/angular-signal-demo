1b2aa2caf16ec5241637739ef5f86036be3cdce0
nishant203 <nishant203@gmail.com>
2023-06-08 09:34:15 +0530

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,16 @@ import { bootstrapApplication } from '@angular/platform-browser';
   template: `
     <div>Count {{count()}}</div>
     <div>Double {{double()}}</div>
-    <
+    <button (click)="changeCount()">Change Count</button>
   `,
 })
 export class App {
-  count = signal(0)
-  double = computed(()=> this.count() *2)
-  name = 'Angular';
+  count = signal(0);
+  double = computed(() => this.count() * 2);
+
+  public changeCount() {
+    this.count.set(5);
+  }
 }
 
 bootstrapApplication(App);
